a51e21e68f77f20d18f7addd81f3aa931f8a853c
Ivan Sebastian <ivansedbert@gmail.com>
2020-07-16 20:39:51 +0700

    Create list done

--- app/src/main/java/com/example/recycleview/MyAdapter.java
+++ app/src/main/java/com/example/recycleview/MyAdapter.java
@@ -1,11 +1,18 @@
 package com.example.recycleview;
 
 import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import static com.example.recycleview.R.id.myImageView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
@@ -23,22 +30,34 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        //holder.myImage.setImageResource(images[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data1.length;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView myText1, myText2;
+        Image myImage;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            myText1 = itemView.findViewById(R.id.myText1);
+            myText2 = itemView.findViewById(R.id.myText2);
+            //myImage = itemView.findViewById(R.id.myImageView);
         }
     }
 }
